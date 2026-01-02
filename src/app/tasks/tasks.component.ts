dd98bfedb393f704b722466d71f7b827e7496a31
Tijana <avramoskatijana@gmail.com>
2026-01-02 15:27:20 +0100

    Section2/NewTaskDialogue

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,17 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
         @Input({required:true}) name!:String;
         @Input({required:true}) id!: string;
+
+        isAddingTask=false;
+
         tasks=[
           {
             id:'t1',
@@ -44,4 +48,12 @@ export class TasksComponent {
         onCompleteTask(id: string){
           this.tasks=this.tasks.filter((task)=>task.id!==id);
         }
+
+        onStartAddTask(){
+          this.isAddingTask=true;
+        }
+        onCancelAddTask(){
+          this.isAddingTask=false;
+        }
+
 }
