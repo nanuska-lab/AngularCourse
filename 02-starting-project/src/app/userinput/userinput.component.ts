57fc4bb582897a6f610404795de951d849ce7358
Tijana <avramoskatijana@gmail.com>
2026-01-09 11:24:55 +0100

    Section4/GettingTheValues

--- 02-starting-project/src/app/userinput/userinput.component.ts
+++ 02-starting-project/src/app/userinput/userinput.component.ts
@@ -6,10 +6,19 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [FormsModule],
   templateUrl: './userinput.component.html',
-  styleUrl: './userinput.component.css'
+  styleUrl: './userinput.component.css',
 })
 export class UserinputComponent {
-    onSubmit(){
-      console.log('submited')
-    }
+  enteredInitialInvestment='0';
+  enteredAnnualInvestment='0';
+  enteredExpectedReturn='5';
+  enteredDuration='10';
+
+  onSubmit() {
+    console.log('submited');
+    console.log(this.enteredAnnualInvestment);
+    console.log(this.enteredDuration);
+    console.log(this.enteredExpectedReturn);
+    console.log(this.enteredInitialInvestment);
+  }
 }
