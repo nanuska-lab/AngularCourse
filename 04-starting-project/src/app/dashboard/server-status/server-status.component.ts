15d50ec8fae2e7e6aaa70c1c77eed72f7a54898f
Tijana <avramoskatijana@gmail.com>
2026-01-22 11:42:24 +0100

    Section6/Lifecycles

--- 04-starting-project/src/app/dashboard/server-status/server-status.component.ts
+++ 04-starting-project/src/app/dashboard/server-status/server-status.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
   imports: [],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  constructor() {}
+
+  ngOnInit() {
+    setInterval(() => {
+      const rand = Math.random(); //0 to 1
+      if (rand < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rand < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
