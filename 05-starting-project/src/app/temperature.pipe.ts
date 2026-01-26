202a1d7ab9ebf266f38a9facc118a40c24f1c21a
Tijana <avramoskatijana@gmail.com>
2026-01-26 10:55:34 +0100

    Section8/CustomPipes

---
+++ 05-starting-project/src/app/temperature.pipe.ts
@@ -0,0 +1,40 @@
+import { output, Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: 'temp',
+  standalone: true,
+})
+export class TemperaturePipe implements PipeTransform{
+    transform(value: string | number, inputType: 'cel' | 'fah', outputType?: 'cel' | 'fah') {
+
+        let val: number;
+
+        if (typeof value === 'string') {
+            val = parseFloat(value);
+        } else {
+            val = value;
+        }
+
+        let outputTemp: number;
+
+        if (inputType === 'cel' && outputType === 'fah') {
+            outputTemp = val * (9/5) + 32; //Celsius to Fahrenheit conversion formula
+        }
+        else if (inputType === 'fah' && outputType === 'cel') {
+            outputTemp = (val - 32) * (5/9); //Fahrenheit to Celsius conversion formula
+        } 
+        else {
+            outputTemp = val; // No conversion needed
+        }
+        
+        let symbol: '°C' | '°F';
+        if(!outputType){
+            symbol = inputType === 'cel' ? '°C' : '°F';
+        }
+        else{
+            symbol = outputType === 'cel' ? '°C' : '°F';
+        }
+
+        return `${outputTemp} ${symbol}`;
+    }
+}
