82beb3f25ce6853754998568005a44cfb8d934f5
Tijana <avramoskatijana@gmail.com>
2026-01-29 14:55:37 +0100

    Section10/ChangeDetection

--- 07-starting-project/src/app/messages/messages-list/messages-list.component.ts
+++ 07-starting-project/src/app/messages/messages-list/messages-list.component.ts
@@ -1,13 +1,18 @@
-import { Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { MessagesService } from '../messages.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MessagesListComponent {
-  messages = input.required<string[]>();
+  private messagesService=inject(MessagesService);
+  messages$=this.messagesService.messages$;
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
